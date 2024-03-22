d237fa232cd580918fefa6f34dd6d1cec3846500
Smitanshu Ukey <150527585+Smitanshu@users.noreply.github.com>
2024-03-22 09:39:26 +0530

    Create check_sorted_recurive.cpp

---
+++ check_sorted_recurive.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+bool check(int arr[], int size, int index)
+{
+
+    // base case
+    if (index >= size)
+    {
+        return true;
+    }
+
+    // processing
+
+    if (arr[index] > arr[index - 1])
+    {
+        // Recursive relation
+        bool nextans = check(arr, size, index + 1);
+    }
+
+    else
+    {
+        return false;
+    }
+}
+
+int main()
+{
+
+    int arr[5] = {0, 20, 30, 40, 50};
+
+    int size = sizeof(arr) / sizeof(arr[0]);
+
+    bool ans = check(arr, size, 1);
+
+    if (ans)
+    {
+        cout << "Array is sorted ";
+    }
+
+    else
+    {
+        cout << "Array is not started";
+    }
+
+    return 0;
+}
