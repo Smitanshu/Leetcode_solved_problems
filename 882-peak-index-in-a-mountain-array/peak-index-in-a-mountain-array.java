2993edbfa4714ec1ba6a45c4647783a3c071fc2a
Smitanshu Ukey <150527585+Smitanshu@users.noreply.github.com>
2024-08-14 12:30:33 +0530

    Time: 1 ms (14.80%) | Memory: 55.9 MB (79.95%) - LeetSync

--- 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
+++ 882-peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -5,10 +5,10 @@ int index=0;
         for(int i=0; i<arr.length;i++){
 
     
-            if(arr[i]>arr[i+1] && i+1<arr.length)
+            if(arr[i]>arr[i+1] && i+1<arr.length){
 
 
-            {index=i;
+            index=i;
             break;
             }
         }
