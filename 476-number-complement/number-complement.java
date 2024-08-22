0537bc03883fcfcbd2ecb51028f8931af962113a
Smitanshu Ukey <150527585+Smitanshu@users.noreply.github.com>
2024-08-22 20:16:25 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (80.96%) - LeetSync

--- 476-number-complement/number-complement.java
+++ 476-number-complement/number-complement.java
@@ -1,14 +1,8 @@
 class Solution {
     public int findComplement(int num) {
         
+    int mask = (Integer.highestOneBit(num) << 1) - 1;
+    return num ^ mask;
+}
 
-        String binary = Integer.toBinaryString(num);
-    StringBuilder complement = new StringBuilder();
-    
-    for (char bit : binary.toCharArray()) {
-        complement.append(bit == '0' ? '1' : '0');
-    }
-    
-    return Integer.parseInt(complement.toString(), 2);
-    }
 }
