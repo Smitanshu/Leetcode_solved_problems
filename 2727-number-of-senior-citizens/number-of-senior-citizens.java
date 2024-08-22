25f53479fbf55b5d6d46c6748b8321e11f0423cd
Smitanshu Ukey <150527585+Smitanshu@users.noreply.github.com>
2024-08-22 22:41:14 +0530

    Time: 1 ms (75.63%) | Memory: 42.1 MB (86.02%) - LeetSync

--- 2727-number-of-senior-citizens/number-of-senior-citizens.java
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.java
@@ -9,14 +9,14 @@ class Solution {
             String curr=details[i];
 
 
-            for(int j=0; j<1;j++){
+            
 
                  int a = Character.getNumericValue(curr.charAt(11)) * 10 + Character.       getNumericValue(curr.charAt(12));
               if(a>60){
                 count++;
               }
 
-            }
+            
         }
        
 
