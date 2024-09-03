88273e26224bae580bad718cfe4031ea3c62a6cf
Smitanshu Ukey <150527585+Smitanshu@users.noreply.github.com>
2024-09-04 02:00:56 +0530

    Time: 1854 ms (11.15%) | Memory: 59.8 MB (63.80%) - LeetSync

---
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int chalkReplacer(int[] chalk, int k) {
+      int ans=0;
+        outer:
+        while(true){
+            
+
+
+
+            for(int i=0; i<chalk.length;i++){
+
+                if(chalk[i]<=k){
+                    k=k-chalk[i];
+                }else{
+                    ans=i;
+                    break outer;
+                }
+            }
+        
+        }
+
+        return ans;
+        
+    }
+}
