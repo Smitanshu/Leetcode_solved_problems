d4bff3dda4d3a5ac15343cf372538a7436a55708
Smitanshu Ukey <150527585+Smitanshu@users.noreply.github.com>
2024-10-08 23:59:28 +0530

    Time: 99 ms (77.02%) | Memory: 32.8 MB (54.72%) - LeetSync

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.cpp
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.cpp
@@ -1,23 +1,45 @@
+// class Solution {
+// public:
+//     int minSwaps(string s) {
+//         stack<char>stk;
+//         int count=0;
+//         for(int i=0; s[i]!='\0';i++){
+
+//            if(s[i]=='['){
+//             stk.push('[');
+//            }else{
+
+//             if(!stk.empty()){
+//                 stk.pop();
+//             }
+//             else{
+//                 count++;
+//             }
+//            }
+//         }
+//         return ((count+1)/2);
+//     }
+// };
+
+// Optimized Approach :
 class Solution {
 public:
     int minSwaps(string s) {
-        stack<char>stk;
-        int count=0;
-        for(int i=0; s[i]!='\0';i++){
 
+        int count = 0;
+        int max_count = 0;
+        for (int i = 0; s[i] != '\0'; i++) {
 
-           if(s[i]=='['){
-            stk.push('[');
-           }else{
-
-            if(!stk.empty()){
-                stk.pop();
+            if (s[i] == '[') {
+                count = count - 1;
+            } else {
+                count = count + 1;
             }
-            else{
-                count++;
+
+            if (max_count < count) {
+                max_count = count;
             }
-           }
         }
-        return ((count+1)/2);
+        return ((max_count + 1) / 2);
     }
 };
