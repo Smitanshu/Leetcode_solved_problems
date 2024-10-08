3fc2eeae9d8c7643695f87c3097117d178adcb78
Smitanshu Ukey <150527585+Smitanshu@users.noreply.github.com>
2024-10-09 00:08:38 +0530

    Time: 18 ms (42.43%) | Memory: 54.3 MB (68.50%) - LeetSync

--- 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.java
+++ 2095-minimum-number-of-swaps-to-make-the-string-balanced/minimum-number-of-swaps-to-make-the-string-balanced.java
@@ -1,45 +1,45 @@
-// class Solution {
-//     public int minSwaps(String s) {
-
-//      int count = 0;
-//         int max_count = 0;
-//         for (int i = 0; i<s.length(); i++) {
-
-//             if (s.charAt(i)== '[') {
-//                 count = count - 1;
-//             } else {
-//                 count = count + 1;
-//             }
-
-//             if (max_count < count) {
-//                 max_count = count;
-//             }
-//         }
-//         return ((max_count + 1) / 2);
-        
-//     }
-// }
-
-
 class Solution {
-public int minSwaps(String s) {
-        Stack<Character>stk=new Stack<>();
-        int count=0;
-        for(int i=0; i<s.length();i++){
+    public int minSwaps(String s) {
 
-           if(s.charAt(i)=='['){
-            stk.push('[');
-           }else{
+     int count = 0;
+        int max_count = 0;
+        for (int i = 0; i<s.length(); i++) {
 
-            if(!stk.empty()){
-                stk.pop();
+            if (s.charAt(i)== '[') {
+                count = count - 1;
+            } else {
+                count = count + 1;
             }
-            else{
-                count++;
+
+            if (max_count < count) {
+                max_count = count;
             }
-           }
         }
-        return ((count+1)/2);
+        return ((max_count + 1) / 2);
+        
     }
-};
+}
+
+
+// class Solution {
+// public int minSwaps(String s) {
+//         Stack<Character>stk=new Stack<>();
+//         int count=0;
+//         for(int i=0; i<s.length();i++){
+
+//            if(s.charAt(i)=='['){
+//             stk.push('[');
+//            }else{
+
+//             if(!stk.empty()){
+//                 stk.pop();
+//             }
+//             else{
+//                 count++;
+//             }
+//            }
+//         }
+//         return ((count+1)/2);
+//     }
+// };
 
